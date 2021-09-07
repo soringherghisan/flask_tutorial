58dd9ed6636900624308e26aa67063977268aa68
soringherghisan <sorin.gherghisan@gmail.com>
2021-09-07 14:31:26 +0300

    - blog zone

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -31,8 +31,11 @@ def create_app(test_config=None):
 
     from . import db
     from . import auth
+    from . import blog
     db.init_app(app)
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
 
